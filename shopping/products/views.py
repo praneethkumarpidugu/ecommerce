42f64c43c98ac86794000f3eec4ea0187b18c9d0
praneeth <Praneeth.Pidugu@windriver.com>
2015-07-31 14:08:02 -0400

    playing around the sample user authentication with views, templates

--- shopping/products/views.py
+++ shopping/products/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    template = 'home.html'
-    context = locals()
+    if request.user.is_authenticated():
+        #sample = "Praneeth Kumar Pidugu"
+        context = {"sample":request.user}
+    else:
+        context = {"sample": request.user}
+    template = 'base.html'
     return render(request, template, context)
