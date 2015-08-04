e568358e2e793dadc8460581601e8fa4d7f40a77
praneeth <Praneeth.Pidugu@windriver.com>
2015-08-04 17:11:53 -0400

    collected static files with the configuration in settings file

--- shopping/shopping/urls.py
+++ shopping/shopping/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,3 +13,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
