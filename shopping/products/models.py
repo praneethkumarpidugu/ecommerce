0eb6c4237f2acf53baa028e32dc39e2368d8f329
praneeth <Praneeth.Pidugu@windriver.com>
2015-08-04 16:06:50 -0400

    added uploadimage class to models.py and admin.py

--- shopping/products/models.py
+++ shopping/products/models.py
@@ -16,3 +16,14 @@ class Product(models.Model):
 
     def get_price(self):
         return self.price
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product)
+    image = models.ImageField(upload_to='products/images/')
+    featured = models.BooleanField(default=False)
+    thumbnail = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __unicode__(self):
+        return self.product.title
