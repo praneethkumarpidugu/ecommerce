451cc4afcf34f1c44028da87037ebda259e58ffa
praneeth <Praneeth.Pidugu@windriver.com>
2015-08-05 16:23:56 -0400

    added unique constraints to the product

--- shopping/products/models.py
+++ shopping/products/models.py
@@ -6,13 +6,16 @@ class Product(models.Model):
     description = models.TextField(null=True, blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=100, default=29.99)
     sale_price = models.DecimalField(decimal_places=2, max_digits=100, null=True, blank=True)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     active = models.BooleanField(default=True)
 
     def __unicode__(self):
         return "This is the Title"
+
+    class Meta:
+        unique_together = ('title', 'slug')
 
     def get_price(self):
         return self.price
