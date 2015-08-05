ecaec115cc96f815955c84393d60c656f143d990
praneeth <Praneeth.Pidugu@windriver.com>
2015-08-05 17:01:30 -0400

    added slug function to view single product

--- shopping/products/views.py
+++ shopping/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
 from .models import Product
 
 # Create your views here.
@@ -13,5 +13,16 @@ def all(request):
     products = Product.objects.all()
     context = {'products': products}
     template = 'products/all.html'
+
+    return render(request, template, context)
+
+def single(request, slug):
+    try:
+        product = Product.objects.get(slug=slug)
+        products = Product.objects.all()
+        context = {'product': product}
+        template = 'products/single.html'
+    except:
+        raise Http404
 
     return render(request, template, context)
