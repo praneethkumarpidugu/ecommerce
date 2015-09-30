ea9d4ddab92f623a062597eff1a29312d19450a9
praneeth <Praneeth.Pidugu@windriver.com>
2015-09-30 15:59:24 -0400

    create a new app orders with orderid, foreignkey to Cart, status, timestamp, updated

---
+++ shopping/orders/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+from carts.models import Cart
+
+STATUS_CHOICES = (
+    ("Started", "Started"),
+    ("Abondoned", "Abondoned"),
+    ("Finished", "Finished"),
+)
+
+class Order(models.Model):
+    #add user
+    #address
+    order_id = models.CharField(max_length=120, default='ABC', unique=True)
+    cart = models.ForeignKey(Cart)
+    status = models.CharField(max_length=120, choices=STATUS_CHOICES, default="Start")
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __unicode__(self):
+        return self.order_id
--- shopping/shopping/settings.py
+++ shopping/shopping/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'gunicorn',
     'products',
     'carts',
+    'orders',
 )
 
 MIDDLEWARE_CLASSES = (
