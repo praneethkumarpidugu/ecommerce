7fb1aeed520b41ebfca4c17972b719995226a980
praneeth <Praneeth.Pidugu@windriver.com>
2015-11-10 12:24:24 -0500

    added user ,sub_total, final_total as the decimal fields into orders

--- shopping/orders/models.py
+++ shopping/orders/models.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
 from carts.models import Cart
+
+User = get_user_model()
 
 STATUS_CHOICES = (
     ("Started", "Started"),
@@ -10,11 +13,15 @@ STATUS_CHOICES = (
 )
 
 class Order(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True)
     #add user
     #address **
     #subtotal
     #tax
     #final price
+    sub_total = models.DecimalField(default=10.99, max_digits=1000, decimal_places=2)
+    tax_total = models.DecimalField(default=0.99, max_digits=1000, decimal_places=2)
+    final_total = models.DecimalField(default=10.99, max_digits=1000, decimal_places=2)
     order_id = models.CharField(max_length=120, default='ABC', unique=True)
     cart = models.ForeignKey(Cart)
     status = models.CharField(max_length=120, choices=STATUS_CHOICES, default="Start")
